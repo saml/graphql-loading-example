84448d3a7309e1e03e3b01e6a037f2c2df939feb
saml <saml>
2022-05-03 10:52:04 -0400

    asdf

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const { buildSubgraphSchema } = require("@apollo/subgraph");
+
 const typeDefs = require('./typeDefs');
+const resolvers = {
+  Query: {
+    query1: (parent, args, context) => {
+      return {
+        id: 'test-category',
+        name: 'Test Category',
+      }
+    }
+  }
+};
+
+buildSubgraphSchema({ typeDefs, resolvers });
--- typeDefs.js
+++ typeDefs.js
@@ -1,8 +1,8 @@
 const path = require("path");
 
-const { loadSchemaSync } = require("@graphql-tools/load");
+const { loadTypedefsSync } = require("@graphql-tools/load");
 const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
 
-module.exports = loadSchemaSync(path.join(__dirname, "schema.subgraph.graphql"), {
+module.exports = loadTypedefsSync(path.join(__dirname, "schema.subgraph.graphql"), {
   loaders: [new GraphQLFileLoader()],
 });
